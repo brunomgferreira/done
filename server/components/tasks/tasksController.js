8ee9b46fbc737a7c308f3b49f115f9e1b7708ec0
brunomgferreira <brunomgferreira17@gmail.com>
2024-02-02 19:24:40 +0000

    Now the user can see repeated tasks

--- server/components/tasks/tasksController.js
+++ server/components/tasks/tasksController.js
@@ -56,6 +56,8 @@ const getAllTasksByDay = async (req, res) => {
     const currentDate = new Date(day);
     const formattedDate = currentDate.toISOString().substring(0, 10);
 
+    await createAllRepeatedTasks(userId, day);
+
     const connection = await pool.getConnection();
     const [tasks] = await connection.execute(
       "SELECT task.id AS taskId, task.user, task.name AS taskName, task.startDate, task.startTime, task.dueDate, task.dueTime, task.location, task.notes, task.finished, task.finishDate, task.finishTime, category.id AS categoryId, category.name AS categoryName, category.color " +
@@ -92,6 +94,215 @@ const getAllTasksByDay = async (req, res) => {
     connection.release();
 
     res.status(StatusCodes.OK).json({ tasks, count: tasks.length });
+  } catch (error) {
+    throw new InternalServerError(error.message);
+  }
+};
+
+const createAllRepeatedTasks = async (userId, day) => {
+  try {
+    const currentDate = new Date(day);
+    const formattedDate = currentDate.toISOString().substring(0, 10);
+
+    const connection = await pool.getConnection();
+    const [tasks] = await connection.execute(
+      "SELECT task.id AS taskId, task.user, task.name AS taskName, task.startDate, task.startTime, task.dueDate, task.dueTime, task.location, task.notes, task.finished, task.finishDate, task.finishTime, task.category AS categoryId " +
+        "FROM task " +
+        "WHERE task.user = ? " +
+        "AND task.finished = FALSE " +
+        "AND task.startDate < ?",
+      [userId, formattedDate]
+    );
+
+    await Promise.all(
+      tasks.map(async (task) => {
+        const [taskNotifications] = await connection.execute(
+          "SELECT taskNotification.notificationID as id, notification.name " +
+            "FROM taskNotification " +
+            "INNER JOIN notification ON taskNotification.notificationID = notification.id " +
+            "WHERE taskNotification.taskID = ?",
+          [task.taskId]
+        );
+
+        const [taskRepeatIntervals] = await connection.execute(
+          "SELECT taskRepeatInterval.repeatIntervalID as id, repeatInterval.name, taskRepeatInterval.originalTaskID, taskRepeatInterval.repeatValidity " +
+            "FROM taskRepeatInterval " +
+            "INNER JOIN repeatInterval ON taskRepeatInterval.repeatIntervalID = repeatInterval.id " +
+            "WHERE taskRepeatInterval.taskID = ? " +
+            "AND taskRepeatInterval.originalTaskID IS NULL " +
+            "AND (taskRepeatInterval.repeatValidity >= ? OR taskRepeatInterval.repeatValidity IS NULL) " +
+            "AND taskRepeatInterval.repeatIntervalID != 1",
+          [task.taskId, formattedDate]
+        );
+        task.notifications = taskNotifications;
+        task.repeatIntervals = taskRepeatIntervals;
+      })
+    );
+
+    const filteredTasks = tasks.filter(
+      (task) =>
+        task.repeatIntervals.length > 0 &&
+        task.repeatIntervals.every(
+          (repeatInterval) => repeatInterval.originalTaskID === null
+        )
+    );
+
+    connection.release();
+
+    await Promise.all(
+      filteredTasks.map(async (task) => {
+        if (await existsRepeatedTask(task, userId, day))
+          await createRepeatedTask(
+            userId,
+            task.taskId,
+            task.taskName,
+            task.categoryId,
+            task.location,
+            task.notifications,
+            task.repeatIntervals,
+            task.notes,
+            formattedDate,
+            task.startTime,
+            task.dueTime
+          );
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const existsRepeatedTask = async (task, userId, day) => {
+  try {
+    const currentDate = new Date(day);
+    const formattedDate = currentDate.toISOString().substring(0, 10);
+    const currentMonthDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth();
+    const currentWeekDay = currentDate
+      .toLocaleDateString("en-US", { weekday: "short" })
+      .toUpperCase();
+
+    const taskDate = new Date(task.startDate);
+    const taskWeekDay = taskDate
+      .toLocaleDateString("en-US", { weekday: "short" })
+      .toUpperCase();
+    const taskMonthDay = taskDate.getDate();
+    const taskMonth = taskDate.getMonth();
+
+    let result = false;
+
+    for (const repeatInterval of task.repeatIntervals) {
+      if (
+        repeatInterval.id === 2 ||
+        (repeatInterval.id === 3 && currentWeekDay === taskWeekDay) ||
+        (repeatInterval.id === 4 && currentMonthDay === taskMonthDay) ||
+        (repeatInterval.id === 5 &&
+          currentMonthDay === taskMonthDay &&
+          currentMonth === taskMonth)
+      ) {
+        result = true;
+        break;
+      }
+    }
+
+    if (!result) return false;
+
+    const connection = await pool.getConnection();
+    const [tasks] = await connection.execute(
+      "SELECT task.id AS taskId, task.user, task.name AS taskName, task.startDate, task.startTime, task.dueDate, task.dueTime, task.location, task.notes, task.finished, task.finishDate, task.finishTime, task.category AS categoryId " +
+        "FROM task " +
+        "WHERE task.user = ? " +
+        "AND task.startDate = ?",
+      [userId, formattedDate]
+    );
+
+    await Promise.all(
+      tasks.map(async (value) => {
+        const [taskRepeatIntervals] = await connection.execute(
+          "SELECT taskRepeatInterval.repeatIntervalID as id, repeatInterval.name, taskRepeatInterval.originalTaskID, taskRepeatInterval.repeatValidity " +
+            "FROM taskRepeatInterval " +
+            "INNER JOIN repeatInterval ON taskRepeatInterval.repeatIntervalID = repeatInterval.id " +
+            "WHERE taskRepeatInterval.taskID = ? " +
+            "AND taskRepeatInterval.originalTaskID = ? " +
+            "AND taskRepeatInterval.repeatIntervalID != 1 ",
+          [value.taskId, task.taskId]
+        );
+
+        if (taskRepeatIntervals.length > 0) {
+          result = false;
+        }
+      })
+    );
+
+    connection.release();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createRepeatedTask = async (
+  userId,
+  originalTaskId,
+  taskName,
+  category,
+  location,
+  notification,
+  repeat,
+  notes,
+  date,
+  startingTime,
+  endingTime
+) => {
+  try {
+    const connection = await pool.getConnection();
+
+    const taskNameValue = taskName === "" ? null : taskName;
+    const locationValue = location === "" ? null : location;
+    const notesValue = notes === "" ? null : notes;
+
+    const [result] = await connection.execute(
+      "INSERT INTO task (user, name, category, startDate, startTime, dueTime, location, notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        userId,
+        taskNameValue,
+        category,
+        date,
+        startingTime,
+        endingTime,
+        locationValue,
+        notesValue,
+      ]
+    );
+
+    const taskId = result.insertId;
+
+    await Promise.all([
+      repeat.forEach(async (repeatInterval) => {
+        await connection.execute(
+          "INSERT INTO taskRepeatInterval (taskID, repeatIntervalID, originalTaskID, repeatValidity) " +
+            "VALUES (?, ?, ?, ?)",
+          [
+            taskId,
+            repeatInterval.id,
+            originalTaskId,
+            repeatInterval.repeatValidity,
+          ]
+        );
+      }),
+
+      notification.forEach(async (notification) => {
+        await connection.execute(
+          "INSERT INTO taskNotification (taskID, notificationID) VALUES (?, ?)",
+          [taskId, notification.id]
+        );
+      }),
+    ]);
+
+    connection.release();
+
+    return taskId;
   } catch (error) {
     throw new InternalServerError(error.message);
   }
