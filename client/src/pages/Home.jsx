b22d874edd054a81e7f159d4f2a7cd8a3724186a
brunomgferreira <brunomgferreira17@gmail.com>
2024-02-11 10:31:35 +0000

    Home page is now responsive

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,72 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import homeBackground from '../assets/homeBackground.png'
+import homeBackgroundSmall from '../assets/homeBackground_small.png'
+import homeBackgroundMedium from '../assets/homeBackground_medium.png'
 import Button from '../components/elements/Button'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
+  const backgroundImage = windowWidth > 1000 ? homeBackground : windowWidth > 800 ? homeBackgroundMedium : homeBackgroundSmall;
+
   return (
     <main>
-        <AuthenticationButtons>
-          <Link to='/login'>
-            <Button
-              $content="Login"
-              $size="small"
-              $shape="squared"
-              $buttonStyle="transparent"
-              $color="white"
-              $fontColor="white"
-              $animation="underline"
-            ></Button>
-          </Link>
-          <Link to='/register'>
-            <Button 
-              $content="Register"
-              $size="small"
-              $shape="squared"
-              $buttonStyle="transparent"
-              $color="white"
-              $fontColor="white"
-              $animation="underline"
-            ></Button>
-          </Link>
-        </AuthenticationButtons>
         <Content>
           <Slogan>Simplify your life.</Slogan>
           <ButtonContainer>
-            <Button
-              $content="Book a Demo"
-              $size="medium"
-              $shape="squared"
-              $buttonStyle="line"
-              $color="white"
-              $fontColor="white"
-              $animation="color"
-            ></Button>
-            <Button 
-              $content="Our solutions"
-              $size="medium"
-              $shape="squared"
-              $buttonStyle="line"
-              $color="white"
-              $fontColor="white"
-              $animation="color"
-            ></Button>
+            <Link to='/login'>
+              <Button
+                $content="Login"
+                $size="medium"
+                $shape="squared"
+                $buttonStyle="line"
+                $color="white"
+                $fontColor="white"
+                $animation="color"
+              ></Button>
+            </Link>
+            <Link to='/register'>
+              <Button 
+                $content="Register"
+                $size="medium"
+                $shape="squared"
+                $buttonStyle="line"
+                $color="white"
+                $fontColor="white"
+                $animation="color"
+              ></Button>
+            </Link>
           </ButtonContainer>
         </Content>
-        <Image src={homeBackground} />
+        <Image src={backgroundImage} />
     </main>
   )
 }
-
-const AuthenticationButtons = styled.div`
-  gap: 2rem;
-  position: fixed;
-  display: flex;
-  flex-direction: row;
-  top: 1%;
-  right: 1%;
-`
 
 const Content = styled.div`
   gap: 2rem;
